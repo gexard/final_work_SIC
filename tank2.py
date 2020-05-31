62a519b51a0ea639f0f97c569b2a802450dd4b55
Gerardo Sanchez <gerardosanchez@pop-os.localdomain>
2020-05-30 21:12:10 -0500

    added files for tanks 2 and 3 with MQTT functionalities

--- tank2.py
+++ tank2.py
@@ -1,17 +1,17 @@
 # Authors: Fatma Nur & Gerardo Sánchez
-# Date created: May 28, 2020
+# Date created: May 30, 2020
 
 import paho.mqtt.client as mqtt
 import time
 
 broker_address = "127.0.0.1"
-publisher_client = mqtt.Client("Tank 1 Publisher")
+publisher_client = mqtt.Client("Tank 2 Publisher")
 publisher_client.connect(broker_address)
 
 def q_received(client,userdata,message):
-    tank1.q = float(message.payload.decode("utf-8"))
+    tank2.h1=float(message.payload.decode("utf-8"))
 
-listener_client = mqtt.Client("Tank 1 listener")
+listener_client = mqtt.Client("Tank 2 listener")
 listener_client.on_message = q_received
 listener_client.connect(broker_address)
 listener_client.loop_start()
@@ -19,15 +19,15 @@ listener_client.loop_start()
 listener_client.subscribe("Tank1/q")
 
 # Tank dynamics
-def tank1():
-    tank1.q = #
-    tank1.h = #
-    publisher_client.publish("Tank1/h1",tank1.h1)
+def tank2():
+    tank2.h1 = #
+    tank2.h2 = #
+    publisher_client.publish("Tank2/h2",tank2.h2)
 
 # Tank parameters
-tank1.q = 0
-tank1.h1 = 0
+tank2.h1 = 0
+tank2.h2 = 0
 
 while True:
-    tank1()
+    tank2()
     time.sleep(0.05)
--- tank3.py
+++ tank3.py
@@ -1,33 +1,33 @@
 # Authors: Fatma Nur & Gerardo Sánchez
-# Date created: May 28, 2020
+# Date created: May 30, 2020
 
 import paho.mqtt.client as mqtt
 import time
 
 broker_address = "127.0.0.1"
-publisher_client = mqtt.Client("Tank 1 Publisher")
+publisher_client = mqtt.Client("Tank 3 Publisher")
 publisher_client.connect(broker_address)
 
 def q_received(client,userdata,message):
-    tank1.q = float(message.payload.decode("utf-8"))
+    tank3.h2 = float(message.payload.decode("utf-8"))
 
-listener_client = mqtt.Client("Tank 1 listener")
+listener_client = mqtt.Client("Tank 3 listener")
 listener_client.on_message = q_received
 listener_client.connect(broker_address)
 listener_client.loop_start()
 
-listener_client.subscribe("Tank1/q")
+listener_client.subscribe("Tank2/h2")
 
 # Tank dynamics
 def tank1():
-    tank1.q = #
-    tank1.h = #
-    publisher_client.publish("Tank1/h1",tank1.h1)
+    tank3.h2 = #
+    tank3.h3 = #
+    publisher_client.publish("Tank3/h3",tank3.h3)
 
 # Tank parameters
-tank1.q = 0
-tank1.h1 = 0
+tank3.h2 = 0
+tank3.h3 = 0
 
 while True:
-    tank1()
+    tank3()
     time.sleep(0.05)
